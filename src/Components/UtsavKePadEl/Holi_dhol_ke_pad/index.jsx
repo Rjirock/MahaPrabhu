5f4050f719a05809e29bc40b56593dedf8bc2662
rajwebitya <rjrajirock@gmail.com>
2025-03-08 00:36:14 +0530

    raj changes

--- src/Components/UtsavKePadEl/Holi_dhol_ke_pad/index.jsx
+++ src/Components/UtsavKePadEl/Holi_dhol_ke_pad/index.jsx
@@ -1,15 +1,58 @@
-import LayoutEl from "../../../Shared/LayoutEl"
-
-
+import React from 'react';
+import LayoutEl from "../../../Shared/LayoutEl";
 
 const Holi_dhol_ke_pad = () => {  
     return (
-        <>
-       <LayoutEl>
-        <h1>Aditya</h1>
-       </LayoutEl>
-        </>
-    )
+        <LayoutEl>
+            {/* Add color to the h1 heading using inline styles */}
+            <h1 style={{ color: '#FF5733', textAlign: 'center', fontFamily: 'Arial, sans-serif' }}>
+                Holi Dhol Ke Pad
+            </h1>
 
+            {/* Render the content directly without extra divs or pre tags */}
+            <div style={{ fontFamily: 'Arial, sans-serif', fontSize: '16px', lineHeight: '1.6', padding: '20px' }}>
+                चैत्र बदी परवा क
+                <br />
+                े दिन होरी डोल क
+                <br />
+                े 
+                <br />
+                पद
+                <br />
+                गोस्वामी श्रीहित हरिवंश महाप्रभु जी क
+                <br />
+                े पद-राग देव गंधार-
+                <br />
+                झूलत दोऊ नवल किशोर ।
+                <br />
+                रजनी जनित रंग सुख सूचत अंग-अंग उठि भोर ॥ १ ॥ 
+                <br />
+                अति अनुराग भरे मिलि गावत सुर मंदर कल घोर।
+                <br />
+                बीच-बीच प्रीतम चित चोरत प्रिया नैन की कोर ॥२॥
+                <br />
+                अबला अति सुक
+                <br />
+                उमार डरत मन वर हिंडोर झकोर ।
+                <br />
+                पुलकि-पुलकि प्रीतम उर लागत दै नव उरज अकोर ॥३॥
+                <br />
+                उरझी विमल माल क
+                <br />
+                कंकण सों क
+                <br />
+                उण्डल सों कच डोर । 
+                <br />
+                बेपथ जुत क्यौं बनैं विवेचत आनन्द बढ्यौ न थोर ॥४॥ 
+                <br />
+                निरखिनिरखि फ
+                <br />
+                उूलत ललितादिक बिबि मुख चन्द्र चकोर ।
+                <br />
+                दै असीस हरिवंश प्रसंसत करि अंचल की छोर ॥५॥१॥
+            </div>
+        </LayoutEl>
+    );
 }
-export default Holi_dhol_ke_pad
+
+export default Holi_dhol_ke_pad;
